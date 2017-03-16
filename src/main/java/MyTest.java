9fc7e12797dcb4b6047949ea7685102368f7f70c
sudhakar_kappala <sudhakar.kappala@gmail.com>
2017-03-17 00:20:45 +0530

    modified

--- src/main/java/MyTest.java
+++ src/main/java/MyTest.java
@@ -4,6 +4,10 @@ public class MyTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("From my class MyTest! hello!!");
+		
+		System.out.println("From my class MyTest! hello!!");
+		
+		
 	}
 
 }
